5859e934a848399e29df9d2ec5278e1d65c1f980
yashmehrotra <yashmehrotra97@gmail.com>
2020-09-12 14:17:11 +0530

    Uploading the code
    Ball movement code

---
+++ Scripts/NetworkManager.cs
@@ -0,0 +1,17 @@
+﻿using Photon.Pun;
+using UnityEngine;
+
+public class NetworkManager : MonoBehaviourPunCallbacks
+{
+
+    void Start()
+    {
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        Debug.Log("We are connected to the " + PhotonNetwork.CloudRegion + " server!");
+    }
+}
+    
